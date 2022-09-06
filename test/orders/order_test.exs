aff0767d8f29e35ba3f2d76964e1dfc008e68ce6
Eric Kreis <erickreis793@gmail.com>
2022-09-06 01:07:36 -0300

    tests: item struct, create: order factory

---
+++ test/orders/order_test.exs
@@ -0,0 +1,38 @@
+defmodule Exlivery.Orders.OrderTest do
+  use ExUnit.Case
+
+  alias Exlivery.Orders.Order
+
+  import Exlivery.Factory
+
+  @params %{
+    user: build(:user),
+    item: [
+      build(:item),
+      build(:item,
+        description: "Temaki",
+        category: :japanese,
+        quantity: 2,
+        unity_price: Decimal.new("22.72")
+      )
+    ]
+  }
+
+  describe "build/2" do
+    test "when params are valid, returns the item" do
+      response = Order.build(@params.user, @params.item)
+
+      expected_response = {:ok, build(:order)}
+
+      assert response == expected_response
+    end
+
+    test "when there is no items in the order, returns an error" do
+      response = Order.build(@params.user, [])
+
+      expected_response = {:error, "Invalid Parameters"}
+
+      assert response == expected_response
+    end
+  end
+end
