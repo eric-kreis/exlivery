c57af7933aedee69158d9e16a79aadd9ea8c1921
Eric Kreis <erickreis793@gmail.com>
2022-09-07 02:08:47 -0300

    update: user agent

--- lib/users/agent.ex
+++ lib/users/agent.ex
@@ -9,9 +9,12 @@ defmodule Exlivery.Users.Agent do
 
   def get(cpf), do: Agent.get(__MODULE__, &get_user(&1, cpf))
 
-  defp get_user(state, cpf) when is_map_key(state, cpf), do: {:error, "User not found"}
-
-  defp get_user(state, cpf), do: {:ok, Map.get(state, cpf)}
+  defp get_user(state, cpf) do
+    case Map.get(state, cpf) do
+      nil -> {:error, "User not found"}
+      user -> {:ok, user}
+    end
+  end
 
   defp update_state(state, %User{cpf: cpf} = user), do: Map.put(state, cpf, user)
 end
