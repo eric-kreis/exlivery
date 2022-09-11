75bc8865f907102f8338b0c32ccd0a77bce05173
Eric Kreis <erickreis793@gmail.com>
2022-09-11 20:55:42 -0300

    update: order report & gitignore

--- lib/orders/report.ex
+++ lib/orders/report.ex
@@ -3,24 +3,50 @@ defmodule Exlivery.Orders.Report do
   alias Exlivery.Orders.Item
   alias Exlivery.Orders.Order
 
-  def create(filename \\ "report.csv") do
-    order_list = build_order_list()
+  def create(filename \\ "report") do
+    case String.contains?(filename, ".") do
+      true ->
+        {:error, "Filename should not contain file extension"}
 
-    File.write!(filename, order_list)
+      false ->
+        order_report = "#{filename}.csv"
+        items_report = "#{filename}_items.csv"
+
+        lines = build_lines()
+
+        File.write!(order_report, lines.order_lines)
+        File.write!(items_report, lines.item_lines)
+
+        {:ok, "Report successfully created"}
+    end
   end
 
-  defp build_order_list() do
+  defp build_lines() do
     OrderAgent.list_all()
-    |> Map.values()
-    |> Enum.map(&order_string/1)
+    |> Enum.map(&create_lines/1)
+    |> Enum.reduce(%{order_lines: [], item_lines: []}, fn elem, acc ->
+      %{
+        order_lines: [acc.order_lines | elem.order_line],
+        item_lines: [acc.item_lines | elem.item_line]
+      }
+    end)
   end
 
-  defp order_string(%Order{user_cpf: cpf, items: items, total_price: total_price}) do
-    items_string = Enum.map(items, &item_string/1)
-    "#{cpf},#{items_string},#{total_price}\n"
+  defp create_lines({order_uuid, %Order{user_cpf: cpf, total_price: total_price, items: items}}) do
+    item_line = Enum.map(items, &create_item_line/1)
+
+    %{
+      order_line: "#{cpf},#{order_uuid},#{total_price}\n",
+      item_line: item_line
+    }
   end
 
-  defp item_string(%Item{category: category, quantity: quantity, unity_price: unity_price}) do
-    "#{category},#{quantity},#{unity_price}"
+  defp create_item_line(%Item{
+         category: category,
+         quantity: quantity,
+         unity_price: unity_price,
+         description: description
+       }) do
+    "#{category},#{description},#{quantity},#{unity_price}\n"
   end
 end
